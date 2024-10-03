6f593698d2b1470f7e02c9e623a51e930c3dd892
Thiago Pereira Cerpa <thiagocerpa52@gmail.com>
2024-10-02 21:47:46 -0300

    codigo da aula

---
+++ aulas/aula06/comando_if.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+    int main() {
+
+//     int numero = 0;
+
+//     printf("Entre com um numero:");
+//     int deu_certo = scanf("%i", &numero);
+
+//     int eh_divisivel_por_2 = numero % 2 == 0;
+//     //if completo
+//     if (eh_divisivel_por_2){
+//       printf("O numero %i eh par!\n", numero);
+    
+//    } else {
+//         printf("O numero %i eh impar!\n", numero);
+//     }
+
+// if aninhado
+//     int idade = 0;
+//         printf("Entre com a sua idade: ");
+//         int deu_certo = scanf("%i", &idade);
+
+//     if (idade < 16 ){
+//        printf("Voce nao pode votar!\n");
+//     }else {
+//     if (idade < 18 || idade > 70) {
+//         printf("Voce pode votar!\n");
+//     }else{
+//         printf("Voce e obrigado a votar\n");
+//     }
+
+
+//     }
+
+// float media = 0.0f;
+// printf("Entre com a media final:");
+// deu_certo = scanf("%f", &media);
+
+// if encadeado
+if (media >= 9.0f && media <= 10.0f){
+    printf("A mencao eh SS\n");
+}else if (media >= 7.0f && media <= 8.9f){
+    printf("A mencao eh MS\n");
+}else if (media >= 5.0f && media <= 6.9f){
+    printf("A mencao eh MM\n");
+}else if (media>= 3.0f && medai <= 4.9f){
+    printf("A mencao eh MI\n");
+}else if (media >= 0.1f && media <= 2.9f){
+    printf("A mencao eh II\n");
+}else if (media = 0.0f){
+    printf("Sem rendimento");
+}
+
+    return 0;
+}
